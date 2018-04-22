a9c47b2579097641554210aab89d3f9f93628810
Hamza <hamza.ali5542@gmail.com>
2018-04-22 15:00:26 +0500

    added a form on upload

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -2,17 +2,19 @@ module.exports = function(app){
     app.get('/home',function(req,res){
         res.render('index.ejs',{check:checkuser(req.user),username:username(req.user)});
     });
-
+    app.get('/upload',function(req,res){
+        res.render('upload.ejs');
+    });
 };
 function checkuser(s){
     if(s)
-        return true
+        return true;
     else
-        return false
+        return false;
 }
 function username(s){
     if (s)
-        return s.firstName
+        return s.firstName;
     else
-        return 'Profile'
+        return 'Profile';
 }
