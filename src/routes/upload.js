0dd232f3bda427d61947b7663d8dd9dcb5fd8520
Hamza <hamza.ali5542@gmail.com>
2018-05-09 01:38:03 +0500

    added voteup-down

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -1,9 +1,11 @@
-var path = require('path');
+var Objectid = require('mongodb').ObjectID;
+var Songs = require('../schemas/songs');
+var async= require('async');
 var Songs = require('../schemas/songs');
 module.exports = function(app,passport){
-    app.post('/uploadsong',function(req,res){
+    app.post('/uploadsong',isLoggedIn,function(req,res){
         var song = new Songs();
-        song.uploader = 'Hamza';
+        song.uploader = req.user.username;
         song.name = req.body.name;
         song.genre = req.body.genre;
         song.description = req.body.description;
@@ -17,6 +19,46 @@ module.exports = function(app,passport){
                 throw err;
             res.send({success:'sucessfully sent'});
         });
+    });
+    app.put('/voteup/:id',function(req,res){
+        var id = new Objectid(req.params.id);
+        async.waterfall([
+            function (callback) {
+                Songs.findById(id,function(err,result){
+                    if (err)
+                        callback(err);
+                    callback(null,result);
+                });
+            },
+            function(user,callback){
+                Songs.updateOne({_id:id},{views: user.views+1},function(err,success){
+                    if(err)
+                        callback(err);
+                    else
+                        callback(null,success);
+                });
+            }
+        ]);
+    });
+    app.put('/votedown/:id',function(req,res){
+        var id = new Objectid(req.params.id);
+        async.waterfall([
+            function (callback) {
+                Songs.findById(id,function(err,result){
+                    if (err)
+                        callback(err);
+                    callback(null,result);
+                });
+            },
+            function(user,callback){
+                Songs.updateOne({_id:id},{views: user.views-1},function(err,success){
+                    if(err)
+                        callback(err);
+                    else
+                        callback(null,success);
+                });
+            }
+        ]);
     });
 };
 function isLoggedIn(req, res, next) {
