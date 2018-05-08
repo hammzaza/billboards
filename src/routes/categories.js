662ec0c3683c51d9a323cb91e280047d57760366
Hamza <hamza.ali5542@gmail.com>
2018-05-08 13:34:43 +0500

    all routes for categories set up

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,104 +1,84 @@
-
-// <li><a href="category1.html">Pop</a></li>
-// <li><a href="category2.html">Hip-Hop</a></li>
-// <li><a href="category3.html">Old School Hip Hop</a></li>
-// <li><a href="category4.html">Trap</a></li>
-// <li><a href="category5.html">Electro</a></li>
-// <li><a href="category6.html">R&B</a></li>
-// <li><a href="category7.html">Techno</a></li>
-// <li><a href="category8.html">Dubstep</a></li>
-// <li><a href="category9.html">Acapella</a></li>
-// <li><a href="category10.html">Tropical</a></li>
-// </ul>
-// <ul class="dropdown-menu" style="left:-160px;">
-// <li><a href="category11.html">Bass House</a></li>
-// <li><a href="category12.html">Dance/ Edm</a></li>
-// <li><a href="category13.html">Instrumental</a></li>
-// <li><a href="category14.html">Jazz</a></li>
-// <li><a href="category15.html">Orchestral</a></li>
-// <li><a href="category16.html">Disco</a></li>
-// <li><a href="category17.html">Heavy Metal</a></li>
-// <li><a href="category18.html">Classical</a></li>
-// <li><a href="category19.html">Salsa</a></li>
-// <li><a href="category20.html">Romantic</a></li>
-// </ul>
-// <ul class="dropdown-menu" style="left:-320px;">
-
-
-// <ul class="dropdown-menu" style="left:-640px;">
-// <li><a href="category41.html">Opera</a></li>
-// <li><a href="category42.html">Chill</a></li>
-// <li><a href="category43.html">Sleep</a></li>
-// <li><a href="category44.html">Exercise</a></li>
-// <li><a href="category45.html">Latin</a></li>
-// <li><a href="category46.html">AudioBook</a></li> 
 module.exports = function(app){
-// <ul class="dropdown-menu" style="left:-480px;">
-// <li><a href="category31.html">Rock</a></li>
-// <li><a href="category32.html">Deep House</a></li>
-// <li><a href="category33.html">World Reggae</a></li>
-// <li><a href="category34.html">Trance</a></li>
-// <li><a href="category35.html">Soul</a></li>
-// <li><a href="category36.html">World Punk Rock</a></li>
-// <li><a href="category37.html">Drum’n Bass</a></li>
-// <li><a href="category38.html">Rythem</a></li>
-// <li><a href="category39.html">Smooth Jazz</a></li>
-// <li><a href="category40.html">Meditation</a></li>
-// </ul>
-    
-    app.get('/rock',function(req,res){
-        //Rock
-        //category31
+    app.get('/pop',function(req,res){
+        //Pop
+        //category1
     });
-    app.get('/deep-house',function(req,res){
-        //Deep House
-        //category32
+    app.get('/hip-hop',function(req,res){
+        //Hip-hop
+        //category2
     });
-    app.get('/world-reggae',function(req,res){
-        //World Reggae
-        //category33
+    app.get('/old-school-hh',function(req,res){
+        //Old School Hip Hop
+        //category3
     });
-    app.get('/trance',function(req,res){
-        //Trance
-        //category34
+    app.get('/trap',function(req,res){
+        //Trap
+        //category4
     });
-    app.get('/trance',function(req,res){
-        //Soul
-        //category35
+    app.get('/electro',function(req,res){
+        //Electro
+        //category5
     });
-    app.get('/punk-rock',function(req,res){
-        //World Punk Rock
-        //category36
+    app.get('/rnb',function(req,res){
+        //R&B
+        //category6
     });
-    app.get('/drum-bass',function(req,res){
-        //Drum'n Bass
-        //category37
+    app.get('/techno',function(req,res){
+        //Techno
+        //category7
     });
-    app.get('/rythem',function(req,res){
-        //Rythem
-        //category38
+    app.get('/dubstep',function(req,res){
+        //Dubstep
+        //category8
     });
-    app.get('/smooth-jazz',function(req,res){
-        //Smooth Jazz
-        //category39
+    app.get('/acapella',function(req,res){
+        //Acapella
+        //category9
     });
-    app.get('/meditation',function(req,res){
-        //Meditation
-        //category40
+    app.get('/tropical',function(req,res){
+        //Tropical
+        //category10
     });
-
-    // <li><a href="category21.html">Funk</a></li>
-// <li><a href="category22.html">Reggae</a></li>
-// <li><a href="category23.html">House</a></li>
-// <li><a href="category24.html">Tango</a></li>
-// <li><a href="category25.html">Blues</a></li>
-// <li><a href="category26.html">Punk Rock</a></li>
-// <li><a href="category27.html">Country</a></li>
-// <li><a href="category28.html">Alternative</a></li>
-// <li><a href="category29.html">Gospel</a></li>
-// <li><a href="category30.html">Indie</a></li>
-// </ul>
-
+    app.get('/bass-house',function(req,res){
+        //Bass House
+        //category11
+    });
+    app.get('/dance',function(req,res){
+        //Dance
+        //category12
+    });
+    app.get('/instrumental',function(req,res){
+        //Instrumental
+        //category13
+    });
+    app.get('/jazz',function(req,res){
+    //     Jazz
+    //category14
+    });
+    app.get('/orchestral',function(req,res){
+    //  Orchestral
+    //category15
+    });
+    app.get('/disco',function(req,res){
+        //Disco
+        //category16
+    });
+    app.get('/heavy-metal',function(req,res){
+        //Heavy Metal
+        //category17
+    });
+    app.get('/classical',function(req,res){
+        //Classical
+        //category18
+    });
+    app.get('/salsa',function(req,res){
+        //Salsa
+        //category19
+    });
+    app.get('/romantic',function(req,res){
+        //Romantic
+        //category20
+    });
     app.get('/funk',function(req,res){
         //Funk
         //category21
@@ -139,65 +119,45 @@ module.exports = function(app){
             //Indie
             //category30
     });
-    app.get('/bass-house',function(req,res){
-            //Bass House
+    app.get('/rock',function(req,res){
+        //Rock
+        //category31
     });
-    app.get('/dance',function(req,res){
-            //Dance
+    app.get('/deep-house',function(req,res){
+        //Deep House
+        //category32
     });
-    app.get('/instrumental',function(req,res){
-            //Instrumental
+    app.get('/world-reggae',function(req,res){
+        //World Reggae
+        //category33
     });
-    app.get('/jazz',function(req,res){
-        //     Jazz
+    app.get('/trance',function(req,res){
+        //Trance
+        //category34
     });
-    app.get('/orchestral',function(req,res){
-        //  Orchestral
+    app.get('/trance',function(req,res){
+        //Soul
+        //category35
     });
-    app.get('/disco',function(req,res){
-            //Disco
+    app.get('/punk-rock',function(req,res){
+        //World Punk Rock
+        //category36
     });
-    app.get('/heavy-metal',function(req,res){
-            //Heavy Metal
+    app.get('/drum-bass',function(req,res){
+        //Drum'n Bass
+        //category37
     });
-    app.get('/classical',function(req,res){
-            //Classical
+    app.get('/rythem',function(req,res){
+        //Rythem
+        //category38
     });
-    app.get('/salsa',function(req,res){
-            //Salsa
+    app.get('/smooth-jazz',function(req,res){
+        //Smooth Jazz
+        //category39
     });
-    app.get('/romantic',function(req,res){
-            //Romantic
-    });
-    app.get('/pop',function(req,res){
-            //Pop
-    });
-    app.get('/hip-hop',function(req,res){
-            //Hip-hop
-    });
-    app.get('/old-school-hh',function(req,res){
-            //Old School Hip Hop
-    });
-    app.get('/trap',function(req,res){
-            //Trap
-    });
-    app.get('/electro',function(req,res){
-            //Electro
-    });
-    app.get('/rnb',function(req,res){
-            //R&B
-    });
-    app.get('/techno',function(req,res){
-            //Techno
-    });
-    app.get('/dubstep',function(req,res){
-            //Dubstep
-    });
-    app.get('/acapella',function(req,res){
-            //Acapella
-    });
-    app.get('/tropical',function(req,res){
-            //Tropical
+    app.get('/meditation',function(req,res){
+        //Meditation
+        //category40
     });
 };
 
