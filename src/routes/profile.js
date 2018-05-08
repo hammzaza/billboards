26a6e0693f6bcabdba9f5f63b808a6194985cac1
Hamza <hamza.ali5542@gmail.com>
2018-05-08 22:57:44 +0500

    added settings route

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,9 @@ module.exports = function(app){
     app.get('/profile',isLoggedIn,function(req,res){
         res.render('profile.ejs',{check:true,username:req.user.firstName});
     });
+    app.get('/settings',isLoggedIn,function(req,res){
+        res.render('profile.ejs',{check:true,username:req.user.firstName});
+    });
 };
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated())
